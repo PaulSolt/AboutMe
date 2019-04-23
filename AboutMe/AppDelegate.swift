97059e07a25ec98cafe010e99043fe9aaec400f3
Paul Solt <PaulSolt@gmail.com>
2019-04-23 12:42:48 -0400

    Added hello statements.

--- AboutMe/AppDelegate.swift
+++ AboutMe/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print("Hello #1")
+        
         return true
     }
 
--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("My name is Paul Solt")
+        print("Hello #2")
     }
 
 
