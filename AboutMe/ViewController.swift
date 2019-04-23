fa3ef22d40bbab014191e49965c699c9fabff5e6
Paul Solt <PaulSolt@gmail.com>
2019-04-23 13:33:03 -0400

    Added the Introduce Yourself button and action.

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -13,12 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("My name is Paul Solt")
-        print("Hello #2")
-        
-        print("Hello #3")
+//         print("My name is Paul Solt")
     }
 
-
+    @IBAction func introduceYourselfButtonPressed(_ sender: Any) {
+        
+        nameLabel.text = "Paul Solt"
+        hobbiesLabel.text = "Pour over coffee, frisbee, running, iOS development"
+        
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hobbiesLabel: UILabel!
 }
 
